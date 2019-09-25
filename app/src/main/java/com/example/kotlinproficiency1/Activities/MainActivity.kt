daa8751d59bd52c9f057f2cb85bf205d2ec7e7ed
d369789 <Joshua.Wallace@team.telstra.com>
2019-09-25 10:07:42 +1000

    Implemented a loading spinner

--- app/src/main/java/com/example/kotlinproficiency1/Activities/MainActivity.kt
+++ app/src/main/java/com/example/kotlinproficiency1/Activities/MainActivity.kt
@@ -2,41 +2,39 @@ package com.example.kotlinproficiency1.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ListView
 import com.beust.klaxon.Klaxon
 import com.example.kotlinproficiency1.Adapters.FactAdapter
 import com.example.kotlinproficiency1.Models.Container
 import com.example.kotlinproficiency1.Models.Fact
 import com.example.kotlinproficiency1.Models.Url
 import com.example.kotlinproficiency1.R
-import com.example.kotlinproficiency1.Services.FactService
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
+import android.view.animation.AlphaAnimation
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
     var factList : ArrayList<Fact> = ArrayList()
     lateinit var adapter : FactAdapter
     private val client = OkHttpClient()
-
+    var inAnimation: AlphaAnimation? = null
+    var outAnimation: AlphaAnimation? = null
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val factListView = findViewById<ListView>(R.id.factListView)
+        var IsBusy : Boolean
         adapter = FactAdapter(this, R.layout.item_list_content, factList)
         factListView.adapter = adapter
-        callUpdatelist()
-        val refreshButton = findViewById<Button>(R.id.refresh_button)
-
-        refreshButton.setOnClickListener{ updateList() }
+        callUpdateList()
+        refresh_button.setOnClickListener{ callUpdateList() }
     }
 
         //This method is more elegate, separating the login into a services class, but it didnt update the UI, so moved to callUpdateList method
-        fun updateList() {
+    /**
+    fun updateList() {
             FactService().getFacts {
                 this@MainActivity.runOnUiThread {
                     factListView.adapter = FactAdapter(
@@ -48,7 +46,14 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-        fun callUpdatelist(){
+    **/
+
+        private fun callUpdateList(){
+            refresh_button.isEnabled = false
+            inAnimation = AlphaAnimation(0f,1f)
+            inAnimation?.duration = 200
+            progressBarHolder.animation = inAnimation
+            progressBarHolder.visibility = View.VISIBLE
             val request = Request.Builder()
                 .url(Url.BASE_URL + Url.URL)
                 .build()
@@ -68,7 +73,13 @@ class MainActivity : AppCompatActivity() {
                             R.layout.item_list_content,
                             factList
                         )
-                        adapter.notifyDataSetChanged()   }
+                        adapter.notifyDataSetChanged()
+                        outAnimation = AlphaAnimation(1f,0f)
+                        outAnimation?.duration = 200
+                        progressBarHolder.animation = outAnimation
+                        progressBarHolder.visibility = View.GONE
+                        refresh_button.isEnabled = true}
+
                 }
             })
         }
